2d3fa0842f8012a067b769f2c7f524a4b2da526c
arkdevelopment <arkdevelop@users.noreply.github.com>
2018-07-04 13:27:18 -0400

    Update Automated_Class_Login_Record_Setup.ps1

--- Automated_Class_Login_Record_Setup.ps1
+++ Automated_Class_Login_Record_Setup.ps1
@@ -1,3 +1,3 @@
-﻿$FilePath = Get-ChildItem -Filter Automated_Class_Login_Record.py -Recurse -ErrorAction SilentlyContinue -Force | % { $_.FullName } | Select-Object -First 1
+﻿$FilePath = Get-ChildItem -Filter Automated_Class_Login_Record.exe -Recurse -ErrorAction SilentlyContinue -Force | % { $_.FullName } | Select-Object -First 1
 Write-Output "File Found at: $FilePath"
 SchTasks /Create /SC WEEKLY /D TUE,THU /TN "Class Recording" /TR "$FilePath" /ST 11:30
