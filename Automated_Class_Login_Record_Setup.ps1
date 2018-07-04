8c24b291f1a94707712b82dbae80a9fb308e12cc
arkdevelopment <arkdevelop@users.noreply.github.com>
2018-07-04 17:09:06 -0400

    Update Automated_Class_Login_Record_Setup.ps1

--- Automated_Class_Login_Record_Setup.ps1
+++ Automated_Class_Login_Record_Setup.ps1
@@ -1,3 +1,19 @@
-﻿$FilePath = Get-ChildItem -Filter Automated_Class_Login_Record.exe -Recurse -ErrorAction SilentlyContinue -Force | % { $_.FullName } | Select-Object -First 1
-Write-Output "File Found at: $FilePath"
-SchTasks /Create /SC WEEKLY /D TUE,THU /TN "Automated Class Recording" /TR "$FilePath" /ST 11:30
+$File = "Automated_Class_Login_Record.exe"
+
+Write-Output "Searching for $File"
+
+$FilePath = Get-ChildItem -Filter $File -Recurse -ErrorAction SilentlyContinue -Force | % { $_.FullName } | Select-Object -First 1
+
+if ($FilePath -ne $null) {
+    if (Test-Path $FilePath) {
+        Write-Output "$File found at: $FilePath"
+        Write-Output "Setting program schedule"
+        SchTasks /Create /SC WEEKLY /D TUE,THU /TN "Automated Class Recording" /TR "$FilePath" /ST 11:30
+    }
+    else {
+        Write-Warning "Error: Could not find $File"
+    }
+}
+else {
+    Write-Warning "Error: Could not find $File"
+}
